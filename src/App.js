8ab422baf1fbd757623b1c0445f142c1a2a41c9b
Andrew Martinez <andy72391@gmail.com>
2024-10-01 10:20:32 -0400

    Added functionality to the new friend form.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
+import { useState } from "react";
 import { Button } from "./components/Button";
 import { FormAddFriend } from "./components/FormAddFriend";
 import { FormSplitBill } from "./components/FormSplitBill";
 import { Friendslist } from "./components/FriendsList";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <Friendslist />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        <Friendslist friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
